7447195cf977a272ebf0e26518f45e6b489713a3
Maina Nganga <ngangamaina091@gmail.com>
2026-04-28 15:32:30 +0300

    fix: migration tables, model fixes

--- Backend/models/ledger.py
+++ Backend/models/ledger.py
@@ -2,11 +2,18 @@ from extensions import db
 from datetime import datetime
 
 class LedgerAccount(db.Model):
+    __tablename__ = "ledger_account"
+
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     type = db.Column(db.String(50), default="USER_WALLET")
 
+    entries = db.relationship("LedgerEntry", backref="account", lazy=True)
+
+
 class LedgerEntry(db.Model):
+    __tablename__ = "ledger_entries"
+
     id = db.Column(db.Integer, primary_key=True)
     account_id = db.Column(db.Integer, db.ForeignKey("ledger_account.id"))
     transaction_id = db.Column(db.Integer)
--- Backend/models/user.py
+++ Backend/models/user.py
@@ -2,6 +2,8 @@ from extensions import db
 from datetime import datetime
 
 class User(db.Model):
+    __tablename__ = "users" 
+    
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(120))
     email = db.Column(db.String(120), unique=True, nullable=False)

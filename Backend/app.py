ba0e0a78b6c92dcdfe45c1d71cda10eb548f21dd
Maina Nganga <ngangamaina091@gmail.com>
2026-05-21 20:33:56 +0300

    fixed few bugs

--- Backend/app.py
+++ Backend/app.py
@@ -43,4 +43,6 @@ app.register_blueprint(Beneficiaries_bp, url_prefix="/api/beneficiaries")
 app.register_blueprint(users_bp, url_prefix="/api/users")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+     import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
--- Backend/start.sh
+++ Backend/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 flask db upgrade
-gunicorn app:app
+gunicorn app:app --bind 0.0.0.0:$PORT --workers 2 --timeout 120
